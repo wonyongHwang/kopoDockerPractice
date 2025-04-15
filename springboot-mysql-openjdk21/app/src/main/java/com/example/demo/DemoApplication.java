31bb2cc524c2c35ffc6366837dcf9b5c13a3ca5f
Wayne Hwang <wonyong@wonyong.net>
2025-04-15 22:25:49 +0900

    Add files via upload

---
+++ springboot-mysql-openjdk21/app/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot with OpenJDK 21 and MySQL!";
+    }
+}
